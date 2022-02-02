7ea8062ae5156d24b3497ea33c99de2adc52d068
Jose-Oquendo <oquendojuanjose@gmail.com>
2022-02-02 18:58:45 -0500

    Creamos las clases Coordinate y Field que son escenciable para modelar

---
+++ field.py
@@ -0,0 +1,21 @@
+class Field:
+
+    def __init__(self):
+        self.homeless_coordinates = {}
+
+    def add_homeless(self, homeless, coordinates):
+        self.Homeless_coordinates[homeless] = coordinates
+
+    def move_homeless(self, homeless):
+        delta_x, delta_y = homeless.walk()
+
+        #path_x.append(path_x[-1] + delta_x)
+        #path_y.append(path_y[-1] + delta_y)
+
+        current_coordinate = self.homeless_coordinates[homeless]
+        new_coordinate = current_coordinate.move(delta_x, delta_y)
+        self.homeless_coordinates[homeless] = new_coordinate
+
+    def get_coordinate(self, homeless):
+        return self.homeless_coordinates[homeless]
+

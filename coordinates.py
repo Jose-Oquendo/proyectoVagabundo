48cf21dff690d3e78917b4ac3b9c76eb34741ada
Jose-Oquendo <oquendojuanjose@gmail.com>
2022-02-04 16:57:37 -0500

    Correccion de codigo

--- coordinates.py
+++ coordinates.py
@@ -8,6 +8,6 @@ class Coordinate:
         return Coordinate(self.x + delta_x, self.y + delta_y)
 
     def distance(self, other_coordenadas):
-        delta_x = self.x - other_coordenadas
-        delta_y = self.y - other_coordenadas
+        delta_x = self.x - other_coordenadas.x
+        delta_y = self.y - other_coordenadas.y
         return(delta_x**2 + delta_y**2)

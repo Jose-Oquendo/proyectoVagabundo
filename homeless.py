b25f24459446ba032ce92de8db01724503ba1bef
Jose-Oquendo <oquendojuanjose@gmail.com>
2022-02-10 13:31:27 -0500

    creacion de la clase righthomeless

--- homeless.py
+++ homeless.py
@@ -1,43 +1,58 @@
 import random
 
 class Homeless:
-
-    def __init__(self, name, x = 0, y = 0):
+    
+    def __init__(self, name,x=0,y=0):
         self.name = name
         self.x = x
         self.y = y
 
-    def position(self):
-        return (self.x, self.y)
+    def posicion (self):
+        return (self.x,self.y)
 
-    def distance_origin(self):
-        return(self.x**2 + self.y**2)
+    def distance(self,other_coordinate):
+        return (self.x**2 + self.y**2)**0.5
 
 class StandarHomeless(Homeless):
+
+    def __innit__(self, name):
+        super().__init__(name)
+
+    def walk(self):
+        dx,dy= random.choice([(1,0),(-1,0),(0,1),(0,-1)])
+        self.x +=dx
+        self.y +=dy
+        return [self.x,self.y]
+
+class ModerateHomeless(Homeless):
 
     def __init__(self, name):
         super().__init__(name)
 
     def walk(self):
-        return random.choice([(1,0),(-1,0),(0,1),(0,-1)])
+        dx,dy = random.choice([(0,2),(0,-2),(2,0),(-2,0)])
+        self.x +=dx
+        self.y +=dy
+        return [self.x,self.y]
 
-class ModerateHomeless(Homeless):
+class leftHomeless(Homeless):
+
     def __init__(self, name):
         super().__init__(name)
 
-    def ModerateWalk(self):
-        dx, dy = random.choice([(2,0),(-2,0),(0,2),(0,-2)])
-        self.x += dx
-        self.y += dy
-        return [self.x, self.y]
+    def walk(self):
+        dx,dy = random.choice([(0,1),(0,-1),(1,0),(-5,0)])
+        self.x +=dx
+        self.y +=dy
+        return [self.x,self.y]
+    
+class RightHomeless(Homeless):
 
-class LeftHomeless(Homeless):
-    def __init__(self, name):
-        super().__init__(name)
-
-    def leftWalk(self):
-        dx, dy = random.choice([(0,1),(-1,0),(0,1),(0,-5)])
-        self.x += dx
-        self.y += dy
-        return [self.x, self.y]
-
+        def __init__(self, name):
+            super().__init__(name)
+        
+        def walk(self):
+            dx,dy = random.choice([(0,5),(0,-1),(5,0),(-1,0)])
+            self.x +=dx
+            self.y +=dy
+            return [self.x,self.y]
